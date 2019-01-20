25e86bf6a92581b902e6e72c67f21e593fee4b68
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-20 02:05:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,11 +18,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
+   return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-    
+
 }
 
 function removeElementFromEndOfArray(array) {
