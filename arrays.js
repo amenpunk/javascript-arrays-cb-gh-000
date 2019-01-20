c84b9a450a709e543a8adad710ec3368b513e090
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-20 02:50:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function addElementToEndOfArray(array,element){
     return newArray;
 }
 
-function destructivelyAddElementToEndOfArray(array){
+function destructivelyAddElementToEndOfArray(array,element){
   array.push(element);
   return array
 }
